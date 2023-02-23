03b35ec62b9f17d8b20406b7bd766a547252a7cd
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-02-23 08:25:37 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AngularFireModule} from '@angular/fire/compat'
-
+/* import { AngularFirestoreModule } from '@angular/fire/compat/firestore/'; */
+import { environment } from 'src/environments/environment.development';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {provideFirestore,getFirestore} from '@angular/fire/firestore'
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    provideFirestore(()=>getFirestore())
+
   ],
   providers: [],
   bootstrap: [AppComponent]
