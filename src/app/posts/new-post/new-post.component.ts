aefad18cf7392d90d70904e9a79dba4eb0991d16
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-02-27 08:15:41 +0530

    add new post implemented

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit {
+  constructor(){}
+
+  ngOnInit():void{}
+
+  onTitleChange(e:any){
+
+    const title = e.target.value
+    let permalink = title.replace(/\s/g,'-')
+    console.log(permalink)
+  }
 
 }
