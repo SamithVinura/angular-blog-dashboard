76aec22a7db37e5d28a9efbc147d2031eb57f17b
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-03-08 08:54:47 +0530

    Login form HTML template and validations done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { NewPostComponent } from './posts/new-post/new-post.component';
 import { AngularEditorModule } from '@kolkov/angular-editor';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireStorageModule} from '@angular/fire/compat/storage';
+import { LoginComponent } from './auth/login/login.component';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { AngularFireStorageModule} from '@angular/fire/compat/storage';
     DashboardComponent,
     CategoriesComponent,
     AllPostComponent,
-    NewPostComponent
+    NewPostComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
